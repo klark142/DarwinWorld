e811c2418a5a4ffc523aa689d8cec49226f32f99
Jakub Karoń <kkaron0313@gmail.com>
2023-12-27 18:23:05 +0100

    refactor: remove unused code

--- src/main/java/agh/ics/oop/model/EquatorPreferred.java
+++ src/main/java/agh/ics/oop/model/EquatorPreferred.java
@@ -9,24 +9,4 @@ public class EquatorPreferred extends AbstractPlants {
     public EquatorPreferred(WorldMap worldMap) {
         super(worldMap);
     }
-
-//    @Override
-//    public boolean canMoveTo(Vector2d position) {
-//        return position.follows(new Vector2d(0, 0)) && position.precedes(new Vector2d(width - 1, height - 1)) && !isOccupied(position);
-//    }
-//
-//    @Override
-//    public Boundary getCurrentBounds() {
-//        Vector2d lowerLeft = new Vector2d(0, 0);
-//        Vector2d upperRight = new Vector2d(width - 1, height - 1);
-//        return new Boundary(lowerLeft, upperRight);
-//    }
-//
-//    @Override
-//    public UUID getId() {
-//        return id;
-//    }
-//
-//
-
 }
